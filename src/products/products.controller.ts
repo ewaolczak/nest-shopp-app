9b4ffcac085abaa016ae4e8a4d89f43f9a6dc91b
Ewa Olczak <ewa.olczak@gmail.com>
2023-07-29 16:01:33 +0200

    Add products endpoints, add orders files

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {
+    this.productsService = productsService;
+  }
+
+  @Get('/')
+  getAll(): any {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  public getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Delete('/:id')
+  deleteById(@Param('id') id: string) {
+    this.productsService.deleteById(id);
+    return { success: true };
+  }
+}
