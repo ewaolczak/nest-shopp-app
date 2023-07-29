b36833899a81a0d04bb6091c16c5baecae1f54e8
Ewa Olczak <ewa.olczak@gmail.com>
2023-07-29 18:43:23 +0200

    Add prisma for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,32 +10,29 @@ import {
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
-import { ParseUUIDPipe } from '@nestjs/common';
 import { UpdateProductDTO } from './dtos/update-product.dto';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private productsService: ProductsService) {
-    this.productsService = productsService;
-  }
+  constructor(private productsService: ProductsService) {}
 
   @Get('/')
   getAll(): any {
     return this.productsService.getAll();
   }
-
   @Get('/:id')
-  getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const prod = this.productsService.getById(id);
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const prod = await this.productsService.getById(id);
     if (!prod) throw new NotFoundException('Product not found');
     return prod;
   }
 
   @Delete('/:id')
-  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.productsService.getById(id))
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.productsService.getById(id)))
       throw new NotFoundException('Product not found');
-    this.productsService.deleteById(id);
+    await this.productsService.deleteById(id);
     return { success: true };
   }
 
@@ -43,16 +40,15 @@ export class ProductsController {
   create(@Body() productData: CreateProductDTO) {
     return this.productsService.create(productData);
   }
-
   @Put('/:id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() productData: UpdateProductDTO
   ) {
-    if (!this.productsService.getById(id))
+    if (!(await this.productsService.getById(id)))
       throw new NotFoundException('Product not found');
 
-    this.productsService.updateById(id, productData);
+    await this.productsService.updateById(id, productData);
     return { success: true };
   }
 }
